dad65dfbb36939c546842ce35d962acc307c0fce
Richard Jennings <richardjennings@gmail.com>
2025-07-10 17:56:34 +0100

    public config

--- server/routes.go
+++ server/routes.go
@@ -689,7 +689,7 @@ func (s *Server) PullHandler(c *gin.Context) {
 			ch <- r
 		}
 
-		regOpts := &registryOptions{
+		regOpts := &RegistryOptions{
 			Insecure: req.Insecure,
 		}
 
@@ -738,7 +738,7 @@ func (s *Server) PushHandler(c *gin.Context) {
 			ch <- r
 		}
 
-		regOpts := &registryOptions{
+		regOpts := &RegistryOptions{
 			Insecure: req.Insecure,
 		}
 
