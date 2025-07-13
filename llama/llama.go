497d58bd8b80cc5a751c3f21fe14b88c2dc2f1ce
Richard Jennings <richardjennings@gmail.com>
2025-07-13 12:54:40 +0100

    allow configuring ubatch, for embeddings this can be set to the whole context or batch size

--- llama/llama.go
+++ llama/llama.go
@@ -87,10 +87,11 @@ type ContextParams struct {
 	c C.struct_llama_context_params
 }
 
-func NewContextParams(numCtx int, batchSize int, numSeqMax int, threads int, flashAttention bool, kvCacheType string) ContextParams {
+func NewContextParams(numCtx int, batchSize int, ubatchSize int, numSeqMax int, threads int, flashAttention bool, kvCacheType string) ContextParams {
 	params := C.llama_context_default_params()
 	params.n_ctx = C.uint(numCtx)
 	params.n_batch = C.uint(batchSize)
+	params.n_ubatch = C.uint(ubatchSize)
 	params.n_seq_max = C.uint(numSeqMax)
 	params.n_threads = C.int(threads)
 	params.n_threads_batch = params.n_threads
